a06eeed598f7bb93548395e1e73599859e10aa4a
Mike.Mueller <michael17mueller@gmail.com>
2019-11-13 13:35:20 -0500

    small push, making notes regarding login/ register tech to be used

--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -3,31 +3,50 @@ import ServiceItem from './ServiceItem';
 import './Subscriptions.css';
 
 const Subscriptions = ({ dbData }) => {
-	const [subList, setSubList] = useState([
+	const [ subList, setSubList ] = useState([
 		<img style={{ width: '260px', margin: '0 auto' }} src={require('../images/spinner.gif')} alt='spinner' />
 	]);
-	const [userInfo, setUserInfo] = useState({});
+	const [ userInfo, setUserInfo ] = useState({});
 
-	const getUserData = () => {
-		try {
-			let userSubIDs = dbData.data.users[0].subs;
-			let subs = dbData.data.services.filter((sub) => {
-				if (userSubIDs.includes(sub.id)) {
-					return sub;
-				}
-			});
-			let userSubs = subs.map((sub) => {
-				return <ServiceItem servName={sub.name} servPrice={sub.price} key={sub.id} />;
-			});
-			setSubList(userSubs);
-			setUserInfo({ name: dbData.data.users[0].name });
-		} catch (error) {
-			console.log(error);
-			setSubList([<h2>{`Services not found :(`}</h2>]);
-		}
-	};
+	// const getUserData = () => {
+	// 	try {
+	// 		let userSubIDs = dbData.data.users[0].subs;
+	// 		let subs = dbData.data.services.filter((sub) => {
+	// 			if (userSubIDs.includes(sub.id)) {
+	// 				return sub;
+	// 			}
+	// 		});
+	// 		let userSubs = subs.map((sub) => {
+	// 			return <ServiceItem servName={sub.name} servPrice={sub.price} key={sub.id} />;
+	// 		});
+	// 		setSubList(userSubs);
+	// 		setUserInfo({ name: dbData.data.users[0].name });
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 		setSubList([<h2>{`Services not found :(`}</h2>]);
+	// 	}
+	// };
 
 	useEffect(() => {
+		const getUserData = () => {
+			try {
+				let userSubIDs = dbData.data.users[0].subs;
+				let subs = dbData.data.services.filter((sub) => {
+					if (userSubIDs.includes(sub.id)) {
+						return sub;
+					}
+				});
+				let userSubs = subs.map((sub) => {
+					return <ServiceItem servName={sub.name} servPrice={sub.price} key={sub.id} />;
+				});
+				setSubList(userSubs);
+				setUserInfo({ name: dbData.data.users[0].name });
+			} catch (error) {
+				console.log(error);
+				setSubList([ <h2>{`Services not found :(`}</h2> ]);
+			}
+		};
+
 		setTimeout(() => {
 			getUserData();
 		}, 800);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
+/*
+	Use Mongoose to access the mongo DB
+	Check DB if user email/ password exist
+
+	Use bcrypt to encrypt password upon registeration
+*/
+
 const Login = () => (
 	<div className='Login'>
 		<Navbar />
